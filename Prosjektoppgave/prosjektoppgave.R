d5f67dc8db2bf0c558cfbb919ac2bbf6eaf52e67
eso086 <92429934+eso086@users.noreply.github.com>
2022-06-05 18:16:20 +0200

    new version sok-1005

--- Prosjektoppgave/prosjektoppgave.R
+++ Prosjektoppgave/prosjektoppgave.R
@@ -1,35 +1,43 @@
+
 getwd()
+install.packages("hrbrthemes")
+install.packages("extrafont")
 library(tidyverse)
 library(janitor)
-
-
+library(lubridate) ## for date time manipulation
+library(scales) ## Formatting numbers and values
+library(hrbrthemes)# For changing ggplot theme
+library(extrafont) # More font options
+library(ggplot2) 
+# importerer datasett (fra kilde)
+#file.choose() for mac -> choose.files() for windows
 app <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
 cCrime <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
 cDemo <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
 cEmp <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
 wSales <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
 wWeather <- read.csv(
-  choose.files(),
+  file.choose(),
   header = T,
   encoding = "utf-8"
 )
@@ -41,13 +49,13 @@ wWeather <- read.csv(
 appCrimeDemoEmp <- app %>%
   dplyr::left_join(
     cCrime,
-    by = c("Store_County"="Ã¯..County_Name")
+    by = c("Store_County"="County_Name")
   ) %>% dplyr::left_join(
     cDemo,
     by = c("Store_County"="County_Name")
   ) %>% dplyr::left_join(
     cEmp,
-    by = c("Store_County"="Ã¯..County_Name")
+    by = c("Store_County"="County_Name")
   )
 
 #wSales <- (app <- cCrime <-cDemo<-cEmp by County Name) by store_num
@@ -57,7 +65,8 @@ salesCountyData <-  wSales %>%
     by = c("Store_num"="Store_Num")
   )
 
-# regner med Weather_Date er antall dager siden 1-1-1960
+#### regner med Weather_Date er antall dager siden 1-1-1960(version 1 av data)
+#endrer navn på date for å joine med SCD...
 weatherDate <- wWeather %>% dplyr::mutate(date = Weather_Date)
 
 
@@ -71,11 +80,9 @@ fullyJoined <- salesCountyData %>% dplyr::left_join(
 
 write.csv(fullyJoined,"fulyJoined.csv", fileEncoding = "utf-8")
 
-#Calculate total profit per store
+#Sjekker profitt og total profitt for hele bedriften.
 profit <- with(fullyJoined, sum(Profit[Store_num >'2']))
 profit
-
-
 profit2 <- with(fullyJoined, sum(Profit[Store_num =='3']))
 profit2
 
@@ -85,9 +92,58 @@ profit2
 proff_store2 <- with(fullyJoined, sum(Profit[Store_num =='2']))
 proff_store2
 
-high_store_profit <- 
+
+store_num_2 <- 
   salesCountyData %>% 
   filter(Store_num == "2") %>% 
+  select("County_Unemployed", "Sales", "Description", "Day","Date","Profit")
+
+#total sales per day()
+sales_by_day <- store_num_2 %>%
+  filter(Month =="4")
+group_by(Month) %>%
+  summarise(Total_Sales=sum(Sales)) %>% 
+  ungroup
+
+##Visualizing summary data -> 
+sales_by_day %>% 
+  ggplot(aes(reorder(Day,Total_Sales),Total_Sales,fill=Day))+
+  geom_col(show.legend = FALSE,color="black")+
+  geom_text(aes(label=comma(Total_Sales)),size=3,hjust=1,color="black")+
+  #scale_y_comma()+
+  #scale_fill_brewer(palette = "Paired")+
+  coord_flip()+
+  theme_classic()+
+  labs(title = "Total Sales breakdown by week(day variable)",x="Day",y= "Total sales")
+
+#filtering for profit  
+profit_by_day <- store_num_2 %>% 
+  group_by(Day) %>%
+  summarise(Total_profit=sum(Profit)) %>% 
+  ungroup
+#plotting for profit each day
+ggplot(data=profit_by_day, aes(x=Day, y=Total_profit, group=1)) +
+  geom_line()+
+  scale_x_continuous(n.breaks = 16)+
+  geom_point()
+
+
+#-------Oppgave 3----------
+#filtering for profit.
+
+
+Sales_pr_store <- wSales %>%
+  filter(Month == "4") %>% 
+  group_by(Store_num) %>%
+  summarise(Total_Sales=sum(Sales)) %>% 
+  ungroup
+
+
+
+sammenligning <- Sales_pr_store %>% 
+  #group_by(Store_num) %>% 
+  summarise(mean(Total_Sales)) %>% 
+  #ungroup()
   
   
   
@@ -98,7 +154,16 @@ high_store_profit <-
   
   
   
-  
+
+
+
+
+
+
+
+
+
+
 
 
 

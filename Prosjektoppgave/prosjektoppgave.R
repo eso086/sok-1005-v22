a78d29ace54793460886cd693a49c6d01aedc6a5
eso086 <92429934+eso086@users.noreply.github.com>
2022-06-06 17:24:27 +0200

    version 06.06.2022

--- Prosjektoppgave/prosjektoppgave.R
+++ Prosjektoppgave/prosjektoppgave.R
@@ -116,8 +116,6 @@ sales_by_day %>%
   ggplot(aes(reorder(Day,Total_Sales),Total_Sales,fill=Day))+
   geom_col(show.legend = FALSE,color="black")+
   geom_text(aes(label=comma(Total_Sales)),size=3,hjust=1,color="black")+
-  #scale_y_comma()+
-  #scale_fill_brewer(palette = "Paired")+
   coord_flip()+
   theme_classic()+
   labs(title = "Total Sales breakdown by week(day variable)",x="Week",y= "Total sales")
@@ -127,7 +125,7 @@ profit_by_day <- store_num_2 %>%
   group_by(Day) %>%
   summarise(Total_profit=sum(Profit)) %>% 
   ungroup
-#plotting for profit each day
+#plotting profit each week
 ggplot(data=profit_by_day, aes(x=Day, y=Total_profit, group=1)) +
   geom_line()+
   scale_x_continuous(n.breaks = 16)+
@@ -142,127 +140,20 @@ store_1week <- wSales %>%
   filter(Store_num == "2", Month == "10") %>% 
   select("Description", "Price","Sold","Sales","Profit","Date","Month","Day")
 
+new_sum <- store_1week %>% 
+  filter(Day == "7") %>% 
+  group_by(Day) %>% 
+  summarise(Total_sales = sum(Sales)) %>% 
+  ungroup()
+new_sum
+#Det totale salget for butikk nummer 2 i uke nummer 7 er 13954 dollar.
 
-
-
-#-------price groups -------
-sales_price_gr <- s %>% 
-  group_by(price_group = ifelse(Price <= 1.0, "price_below_$1", 
-                                ifelse(Price > 1 & Price <= 2, "price_$1", 
-                                       ifelse(Price > 2 & Price <= 3, "price_$2", 
-                                              ifelse(Price > 3 & Price <= 4, "price_$3", 
-                                                     ifelse(Price > 4 & Price <= 5, "price_$4", 
-                                                            ifelse(Price > 5 & Price <= 6, "price_$5", 
-                                                                   ifelse(Price > 6 & Price <= 7, "price_$6", 
-                                                                          ifelse(Price > 7 & Price <= 8.0, "price_$7", "price_over_$8"))))))))) %>% 
-  summarise(Sold, Price, Sales, Profit)
-
-
-
-#--------l�nt kode fra k-------
-S2 <- fullyJoined %>% 
-  filter(Store_num == "2", Year == "2012")
-
-OS <- fullyJoined %>% 
-  filter(Store_num !="2", Year == "2012")
-
-S2agg <- 
-  aggregate(cbind(Profit, Sold, Margin) ~ Week, S2, sum)
-
-OSagg <- 
-  aggregate(cbind(Profit, Sold, Margin) ~ Week, OS, sum) %>% 
-  mutate(Profit = Profit/9,
-         Sold = Sold/9,
-         Margin = Margin/9)
-
-ggplot() + 
-  geom_line(data =  S11agg, aes(x = Week, y = Profit), color = "red") +
-  geom_line(data = OSagg, aes(x = Week, y = Profit), color = "blue") +
-  labs(title = "Butikk nr. 11 sin ukentlig fortjenestes for 2012 
-       sammenliknet med de andre butikkene", 
-       x = "Uke", y = "Profitt") +
-  scale_x_continuous(breaks = seq(1, 52, 4)) +
-  scale_y_continuous(breaks = seq(1000, 8000, 1000))+
-  annotate("text", x= 43, y =7500, #lager "tekst-bokser" med "annotate" og bestemmer dere plasseringer, og deretter skriver inn teksten den skal inneholde
-           label = "↓ Mean weekly profit in other stores",
-           col = "black", #velger farge og størrelse på tekst
-           size = 4) +
-  annotate("text", x= 41, y =2500, 
-           label = "    ↑  Weekly profit River City Strip Mall",
-           col = "black",
-           size = 4)+
-  theme_get()
+#finne St�rste bidragsytere til salget.
 
 
 
 
-s11u23 <- BigData %>%
-  filter(Store_num == "11", Week == "23")
-
-s11u23 %>% 
-  ggplot(aes(x = Description, y = Sold)) +
-  geom_bar(size = 0.3, color = 'red', position = 'dodge', stat = 'identity') +
-  theme(axis.text.x = element_text(angle = 90, size = 3)) +
-  theme(axis.text.y = element_text(angle = 90, size = 8)) +
-  labs( x = 'varer', y = 'antall varer solgt', title = 'jau')
-
-s11u23 %>% 
-  ggplot(aes(x = Description, y = Week)) +
-  geom_point(size = s11u23$Sold/100, color = 'red') +
-  theme(axis.text.x = element_text(angle = 90, size = 3)) +
-  theme(axis.text.y = element_text(angle = 90, size = 8)) +
-  labs( x = 'varer', y = 'antall varer solgt', title = 'jau')
-
-ggplot() +
-  geom_jitter(data=s11u23, aes(x='', y='', size = s11u23$Sold, col = s11u23$Sold, label = s11u23$Description))
-
-ggplot(s11u23, aes(x= '', y= '', colour=s11u23$Sold, label=s11u23$Sold))+
-  geom_point() +geom_text(hjust=0, vjust=0)+
-  geom_jitter()
-
-
-ggplot() + 
-  geom_line(data =  S11agg, aes(x = Week, y = Profit), color = "red") +
-  geom_line(data = OSagg, aes(x = Week, y = Profit), color = "blue") +
-  labs(title = "Butikk nr. 11 sin ukentlig fortjenestes for 2012 
-       sammenliknet med de andre butikkene", 
-       x = "Uke", y = "Profitt") +
-  scale_x_continuous(breaks = seq(1, 52, 4)) +
-  scale_y_continuous(breaks = seq(1000, 8000, 1000))+
-  annotate("text", x= 43, y =7500, #lager "tekst-bokser" med "annotate" og bestemmer dere plasseringer, og deretter skriver inn teksten den skal inneholde
-           label = "↓ Mean weekly profit in other stores",
-           col = "black", #velger farge og størrelse på tekst
-           size = 4) +
-  annotate("text", x= 41, y =2500, 
-           label = "    ↑  Weekly profit River City Strip Mall",
-           col = "black",
-           size = 4)+
-  theme_get()
-
-
-
-
-P1 <- 
-  ggplot() +
-  geom_line(data =  S11agg, aes(x = Week, y = Sold), color = "red") +
-  geom_line(data = OSagg, aes(x = Week, y = Sold), color = "blue") +
-  scale_x_continuous(breaks = seq(13, 52, 5)) +
-  theme_bw()
-
-
-
-
-P2 <- 
-  ggplot() +
-  geom_line(data =  st2ag, aes(x = Week, y = Cost), color = "red") +
-  geom_line(data = otherag, aes(x = Week, y = Cost), color = "blue") +
-  scale_x_continuous(breaks = seq(13, 52, 8)) +
-  theme_bw()
-
-
-
-
-plot_grid(P1, P2, labels = c("Ukentlig salg", "Ukentlige kostnader"), label_size = 10)
+  
 
 
 #-------Oppgave 3----------
